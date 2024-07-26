b7cef095ef8e28f2de47d3de6a463d4c01906d5c
ravikumar <ravikelakam@gmail.com>
2024-07-26 10:02:22 +0530

    first commit

---
+++ src/test/java/project1/test01/AppTest.java
@@ -0,0 +1,21 @@
+package project1.test01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("testing application.. ");
+    }
+        
+    
+    
+}
